0a16d112ef8beb059f2de65cb1a484ccb9eebf8a
Atharva <atharvajoshi202@gmail.com>
2022-10-22 19:47:58 +0530

    Complete upto hashing(hashing is remaining)

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const validator = require("validator");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 3,
   },
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: [true, "Email is already present"],
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
+    },
   },
   password: {
     type: String,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// Create a user using POST "/api/auth" Doesn't require auth
+// Create a user using POST "/api/auth/createuser" Doesn't require auth
 
-router.post("/", (req, res) => {
-  const user = User(req.body);
-  user.save();
-  res.status(200).send(req.body);
+router.post("/createuser", async (req, res) => {
+  try {
+    const user = User(req.body);
+    const createUser = await user.save();
+    res.status(201).send(req.body);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 module.exports = router;
