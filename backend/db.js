6f8e309c0244bc2c77a01ab352c4ee340a3385f5
Atharva <atharvajoshi202@gmail.com>
2022-12-05 20:02:32 +0530

    Local database added

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://atharva:QQd7J4jL8Iq75MiY@cluster0.jishpco.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  // .connect(
+  //   "mongodb+srv://atharva:QQd7J4jL8Iq75MiY@cluster0.jishpco.mongodb.net/?retryWrites=true&w=majority",
+  //   {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   }
+  // )
+  .connect("mongodb://localhost:27017/inotebook", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to mongo");
   })
